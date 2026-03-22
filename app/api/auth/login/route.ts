54dabcf8292c6552507cc865072f5c14f58a89b7
Bikash Roy <64064624+bitborn@users.noreply.github.com>
2026-03-22 18:01:42 +0530

    Added auth logic

---
+++ app/api/auth/login/route.ts
@@ -0,0 +1,21 @@
+import { NextResponse, NextRequest } from "next/server";
+import { login } from "@/modules/services/auth";
+
+export async function POST(request: NextRequest) {
+    try {
+        const { email } = await request.json();
+        if (!email) {
+            return NextResponse.json({ error: "Email is required" }, { status: 400 });
+        }
+        
+        const token = await login(email);
+        return NextResponse.json({ token }, { status: 200 });
+
+    } catch (error) {
+        if (error instanceof Error && error.message === "Invalid email") {
+            return NextResponse.json({ error: error.message }, { status: 401 });
+        }
+
+        return NextResponse.json({ error: "Internal server error" }, { status: 500 });
+    }
+}
