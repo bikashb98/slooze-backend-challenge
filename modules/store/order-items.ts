79d6b47a2b306e0c0e80072d243e58ee0ac86bf8
Bikash Roy <64064624+bitborn@users.noreply.github.com>
2026-03-22 23:35:12 +0530

    Added order amount calculation logic

--- modules/store/order-items.ts
+++ modules/store/order-items.ts
@@ -1,12 +1,33 @@
 import {prisma} from "@/lib/prisma";
 
 export const addItemToOrder = async (orderId: string, menuItemId: string, quantity: number) => {
+
+    const menuItem = await prisma.menuItem.findUnique({
+        where: { id: menuItemId },
+    });
+
+    if (!menuItem) {
+        throw new Error("Menu item not found");
+    }
+
      const Item = await prisma.orderItem.create({
         data: {
             orderId,
             menuItemId,
-            quantity
+            quantity,
+            price: menuItem.price
         }
+    });
+
+    const items = await prisma.orderItem.findMany({
+        where: { orderId },
+    });
+
+    const totalAmount = items.reduce((sum, item) => sum + item.price * item.quantity, 0);
+
+    await prisma.order.update({
+        where: { id: orderId },
+        data: { amount: totalAmount },
     });
     return Item;
 }
