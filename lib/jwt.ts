6df08522eef8450730c18234f2e11a9ffb9837c1
Bikash Roy <64064624+bitborn@users.noreply.github.com>
2026-03-22 17:29:13 +0530

    Added jwt client

---
+++ lib/jwt.ts
@@ -0,0 +1,15 @@
+import jwt from "jsonwebtoken";
+const secretKey = process.env.JWT_SECRET;
+
+
+export const signToken = (payload: object) => {
+    return jwt.sign(payload, secretKey!, { expiresIn: "1h" });
+}
+
+export const verifyToken = (token: string) => {
+    try {
+        return jwt.verify(token, secretKey!);
+    } catch (err) {
+        throw new Error("Invalid token", { cause: err });
+    }
+}
